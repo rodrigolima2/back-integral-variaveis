e7abd78f545993a4853e5f0bfad297e35a52db76
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-10 18:07:18 -0300

    Exercícios

---
+++ classe-01/index.js
@@ -0,0 +1,5 @@
+let imc = 0, peso = 70, altura = 1.80;
+
+imc = peso / (Math.pow(altura, 2));
+
+console.log(`O IMC desta pessoa é de ${imc}`);
---
+++ classe-02/index.js
@@ -0,0 +1,5 @@
+let fahrenheit = 100, celsius = 0;
+
+celsius = (fahrenheit-32) * (5/9);
+
+console.log(`${fahrenheit} graus Fahrenheit são ${celsius} graus Celsius`);
