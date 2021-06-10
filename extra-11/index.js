840277710c10053f09242ed37d9970301b41f574
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-10 20:46:42 -0300

    Volume de uma esfera

---
+++ extra-11/index.js
@@ -0,0 +1,6 @@
+let volumeEsfera = 0, raio = 0, diametro = 6;
+
+raio = diametro / 2;
+volumeEsfera = (4 * Math.pow(raio, 3)) / 3;
+
+console.log(`O volume de uma esfera de raio ${raio} é ${volumeEsfera} PI`);
