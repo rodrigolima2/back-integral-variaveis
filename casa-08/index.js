dd691b06de657f2e476506010dd35317a83facd7
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-10 20:15:09 -0300

    Calculo do perímetro e área de um círculo

---
+++ casa-08/index.js
@@ -0,0 +1,8 @@
+const PI = 3.14;
+
+let = raio = 10, comprimento = 0, area = 0;
+
+comprimento = 2 * PI * raio;
+area = PI * (Math.pow(raio, 2));
+
+console.log(`O comprimento do circulo é de ${comprimento} e a area é de ${area}`);
