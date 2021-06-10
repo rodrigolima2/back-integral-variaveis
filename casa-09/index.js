9d68f1e6ea3053f5326aff47bf9ffdc3167e47a8
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-10 20:23:26 -0300

    Soma dos ângulos internos de um polígono

---
+++ casa-09/index.js
@@ -0,0 +1,7 @@
+let numeroDeLados = 6, somaAngulos = 0, valorCadaAngulo;
+
+somaAngulos = (numeroDeLados - 2) * 180;
+valorCadaAngulo = somaAngulos / numeroDeLados;
+
+
+console.log(`a soma dos angulos é ${somaAngulos} cada angulo vale ${valorCadaAngulo}`);
